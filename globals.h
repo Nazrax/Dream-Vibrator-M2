93f1134d33fe643b02270a42d55c59cf1e1a9ad7
Adrian Abraham <aabraham@us.ibm.com>
2010-12-03 14:29:09 -0500

    Add calibration code; start to set up serial

---
+++ globals.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#define F_CPU 3686400
+#define BAUD_VALUE 1 // 230.4k @  3.6864MHz
+
+typedef enum {false, true} bool_t;
+
+#endif
--- main.c
+++ main.c
@@ -1,13 +1,19 @@
-#define F_CPU 1000000
+#include "globals.h"
 
-#include "main.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
 
 // Global variables
 uint64_t clock;
+bool_t doReading;
 
 // Function prototypes
 void init_io(void);
 void init_clock(void);
+void init_usart(void);
+
+void DoCalibrate(void);
 
 void init_io() {
   DDRB = _BV(DDB2); // Set SS as output
@@ -15,10 +21,42 @@ void init_io() {
 
 void init_clock() {
   ASSR |= _BV(AS2); // Enable async clock
-  TCCR2A |= _BV(WGM12); // CTC mode
+  TCCR2A |= _BV(WGM21); // CTC mode
   OCR2A = 1; // Fire every other clock tick (16 times / second)
   TCCR2B |= _BV(CS22) | _BV(CS21) | _BV(CS20); // Clock / 1024
   TIMSK2 |= _BV(OCIE2A); // Enable CTC interrupt for OCR2A
+}
+
+inline void init_usart(void) {
+  UBRR0 = BAUD_VALUE;
+  UCSR0C = _BV(UCSZ00) | _BV(UCSZ01); // 8-bit, 1 stop bit, no parity
+  UCSR0B = _BV(TXEN0) | _BV(TXCIE0) | _BV(RXEN0) | _BV(RXCIE0); // Turn on TX and RX and their interrupts
+  UCSR0A = _BV(U2X0); // Double serial clock
+}
+
+
+int main(int argc, char** argv) {
+  CLKPR = (1<<CLKPCE);        // set Clock Prescaler Change Enable
+  CLKPR = _BV(CLKPS0); // Divide by 2, 4 MHz
+  DoCalibrate();
+
+  init_io();
+  init_clock();
+
+  sei();
+
+  // Put accelerometer in SPI mode
+
+  while(true) {
+    if (doReading) {
+      // Get readings from accelerometer
+      // Print them
+
+      doReading = false;
+    }
+  }
+
+  return 0;
 }
 
 // The clock
@@ -26,9 +64,4 @@ ISR(TIMER2_COMPA_vect) {
   clock++;
 }
 
-int main(int argc, char** argv) {
-  init_io();
-  init_clock();
 
-  return 0;
-}
